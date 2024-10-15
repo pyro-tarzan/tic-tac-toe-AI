2aa26b9fbd57515d82c41e738b4aedbfdccb70d2
pyro-tarzan <vickybw16007@gmail.com>
2024-10-15 13:59:58 +0530

    Fix minmax algorithm

--- main.py
+++ main.py
@@ -2,53 +2,75 @@ from singleplayer import SinglePlayer
 from multiplayer import Multiplayer
 from os import system
 
+mp = Multiplayer()
 print("Welcome to Tic Tac Toe.")
 print("Choose mode:\n\tSingle Player: 0\n\tMutiplayer: 1")
-mp = Multiplayer()
 
 is_valid_mode = False
 while not is_valid_mode:
-    player_method = int(input())
-    if player_method == 0:
-        is_valid_mode = True
-        sp = SinglePlayer()
 
-        cpu = None
-        player = ""
-        while cpu is None:
-            player = input("choose x or o: ")
-            cpu = sp.cpu_choice(player)
+    try:
+        player_method = int(input())
+        if player_method == 0:
+            is_valid_mode = True
+            sp = SinglePlayer()
 
-        while not sp.is_game_over:
-            if player == "x":
-                # Start from player
-                sp.print_list()
-                sp.move_player(player)
-                sp.print_list()
-                sp.move_cpu(cpu, player)
+            cpu = None
+            player = ""
+            while cpu is None:
+                player = input("choose x or o: ")
+                cpu = sp.cpu_choice(player)
 
-            else:
-                # Start from CPU
-                # sp.move_cpu(cpu, player)
-                pass
+            while True:
+                if player == "x":
+                    # Start from player
+                    sp.print_list()
+                    if sp.check_win(sp.updatable_list) == player:
+                        print(f"Player Win.")
+                        break
+                    elif sp.check_win(sp.updatable_list) == cpu:
+                        print("CPU Win.")
+                        break
+                    elif sp.check_for_draw():
+                        print("Match Draw.")
+                        break
+                    sp.move_player(player)
+                    sp.print_list()
+                    if sp.check_win(sp.updatable_list) == player:
+                        print(f"Player Win.")
+                        break
+                    elif sp.check_win(sp.updatable_list) == cpu:
+                        print("CPU Win.")
+                        break
+                    elif sp.check_for_draw():
+                        print("Match Draw.")
+                        break
+                    sp.move_cpu(cpu, player)
 
-    elif player_method == 1:
-        is_valid_mode = True
-        mp = Multiplayer()
+                else:
+                    # Start from CPU
+                    pass
 
-        while not mp.is_game_over:
-            system("cls")
-            mp.print_list()
-            if mp.check_win("Player_2"):
-                break
+        elif player_method == 1:
+            is_valid_mode = True
+            mp = Multiplayer()
 
-            player_1 = mp.player_turn("x", "Player_1")
+            while not mp.is_game_over:
+                system("cls")
+                mp.print_list()
+                if mp.check_win("Player_2"):
+                    break
 
-            system("cls")
-            mp.print_list()
-            if mp.check_win("Player_1"):
-                break
+                player_1 = mp.player_turn("x", "Player_1")
 
-            player_2 = mp.player_turn("o", "Player_2")
-    else:
-        print(f"Please a enter a valid mode.")
+                system("cls")
+                mp.print_list()
+                if mp.check_win("Player_1"):
+                    break
+
+                player_2 = mp.player_turn("o", "Player_2")
+        else:
+            print(f"Please a enter a valid mode.")
+    except ValueError:
+        system("cls")
+        print("\"INVALID INPUT\"\nChoose mode:\n\tSingle Player: 0\n\tMutiplayer: 1")
--- singleplayer.py
+++ singleplayer.py
@@ -9,27 +9,33 @@ class SinglePlayer:
                             [4, 5, 6],
                             [7, 8, 9]]
 
-        self.is_game_over = False
+    def check_for_draw(self):
+        board = self.updatable_list
+        for row in range(len(board)):
+            for col in range(len(board[row])):
+                if board[row][col] == "_":
+                    return False
+        return True
 
     def check_win(self, board):
         for row_index in range(0, 3):
-            if (board[row_index][0] == board[row_index][1] and
+            if (board[row_index][0] != "_" and board[row_index][0] == board[row_index][1] and
                     board[row_index][1] == board[row_index][2]):
 
                 return board[row_index][0]
 
         for col_index in range(0, 3):
-            if (board[0][col_index] == board[1][col_index] and
+            if (board[0][col_index] != "_" and board[0][col_index] == board[1][col_index] and
                     board[1][col_index] == board[2][col_index]):
 
                 return board[0][col_index]
 
-        if (board[0][0] == board[1][1] and
+        if (board[0][0] != "_" and board[0][0] == board[1][1] and
                 board[1][1] == board[2][2]):
 
             return board[0][0]
 
-        if (board[0][2] == board[1][1] and
+        if (board[0][2] != "_" and board[0][2] == board[1][1] and
                 board[1][1] == board[2][0]):
 
             return board[0][2]
@@ -51,24 +57,32 @@ class SinglePlayer:
 
     def move_player(self, player):
         player_turn = int(input("Player: "))
-
-        for row in range(0, len(self.placeholder)):
-            for col in range(0, len(self.placeholder[row])):
-                if player_turn == self.placeholder[row][col]:
-                    self.updatable_list[row][col] = player
-
+        if 1 <= player_turn < 10:
+            for row in range(0, len(self.placeholder)):
+                for col in range(0, len(self.placeholder[row])):
+                    if player_turn == self.placeholder[row][col]:
+                        if self.updatable_list[row][col] == "_":
+                            self.updatable_list[row][col] = player
+                        else:
+                            self.move_player(player)
+        else:
+            print("Please enter a valid range.")
+            self.move_player(player)
 
     def move_cpu(self, cpu, player):
         row, col = self.best_move(self.updatable_list, player, cpu)
-        print(row, col)
-        self.updatable_list[row][col] = cpu
+        print(f"CPU Places: {row}, {col}")
+        if self.updatable_list[row][col] == "_":
+            self.updatable_list[row][col] = cpu
+        else:
+            print("already occupied.")
 
     def minmax(self,board , depth, is_maximizing, player, cpu):
         winner = self.check_win(board)
-        if winner == "x":
-            return 10
-        elif winner == "o":
-            return -10
+        if winner == player:
+            return -10 + depth
+        elif winner == cpu:
+            return 10 - depth
         elif all(cell != "_" for row in board for cell in row):
             return 0
 
@@ -77,7 +91,7 @@ class SinglePlayer:
             for i in range(3):
                 for j in range(3):
                     if board[i][j] == "_":
-                        board[i][j] = player  # X's turn
+                        board[i][j] = cpu  # X's turn
                         score = self.minmax(board, depth + 1, False, player, cpu)
                         board[i][j] = "_"
                         best_score = max(score, best_score)
@@ -87,7 +101,7 @@ class SinglePlayer:
             for i in range(3):
                 for j in range(3):
                     if board[i][j] == "_":
-                        board[i][j] = cpu  # O's turn
+                        board[i][j] = player  # O's turn
                         score = self.minmax(board, depth + 1, True, player, cpu)
                         board[i][j] = "_"
                         best_score = min(score, best_score)
@@ -100,7 +114,7 @@ class SinglePlayer:
         for i in range(3):
             for j in range(3):
                 if board[i][j] == "_":
-                    board[i][j] = player  # Assume X is the player
+                    board[i][j] = cpu  # Assume X is the player
                     score = self.minmax(board, 0, False, player, cpu)
                     board[i][j] = "_"
                     if score > best_score:
